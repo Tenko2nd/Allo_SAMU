fc76a1751d618b49a12a3456f344142d745b2400
Mae Mumi <maeva.ramamonjyramboa@reseau.eseo.fr>
2025-03-18 10:37:43 +0100

    ressources + change preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -1,61 +1,64 @@
 import docx
-import re
 import spacy
 
-# Ouvre le document
-docsPath = r"C:\Projet_synthèse\Data\Retranscriptions_anonymes/"
-record_ID = "A1_2023002752-3445953_2023-01-03_15-54-29_2023-01-03_15-55-57_Entrant.mp3.docx"
-doc = docx.Document(docsPath+record_ID)
 
-# Ouvre le modèle de SpaCy
-nlp = spacy.load("fr_core_news_lg")
+def docx_to_list(record_ID):
+    docsPath = r'C:\Users\maeva\Document\ESEO\E4\S2/'
+    doc = docx.Document(docsPath + record_ID)
 
-# Ouvre les stopwords et caractères spéciaux
-stopwordsPath = r"C:\Projet_synthèse\Ressources\stopwords-fr.txt"
-with open(stopwordsPath, 'r', encoding='utf-8') as f:
-    stopwords = f.read()
-caracterePath = r"C:\Projet_synthèse\Ressources\caracteres_speciaux.txt"
-with open(caracterePath, 'r', encoding='utf-8') as f:
-    caracteres = f.read()
+    # Ouvre les stopwords et caractères spéciaux
+    stopwordsPath = r"Ressources\stopwords-fr.txt"
+    with open(stopwordsPath, 'r', encoding='utf-8') as f:
+        stopwords = f.read()
+    caracterePath = r"Ressources\caracteres_speciaux.txt"
+    with open(caracterePath, 'r', encoding='utf-8') as f:
+        caracteres = f.read()
 
-# met le texte du document dans une liste
-allText = [docpara.text for docpara in doc.paragraphs]
-allText[0] = " ".join(allText[0].split('\n')[3:])
+    # met le texte du document dans une liste
+    allText = [docpara.text for docpara in doc.paragraphs]
+    allText[0] = " ".join(allText[0].split('\n')[3:])
+
+    return stopwords, caracteres, allText
 
 
-def clean_and_lemmatize(text, stopwords):
+def clean_and_lemmatize(text, caracteres, stopwords, nlp): # Passer nlp_model en argument
     """
     reformate le texte en token et les lemmatize
     :param text:
+    :param caracteres:
     :param stopwords:
+    :param nlp_model: Le modèle SpaCy pré-chargé
     :return:
     """
-    # conversion en minuscules et retire les ponctuations
+
     text = text.lower()
     text = "".join([char if char not in caracteres else ' ' for char in text])
 
-    # Suppression des stopwords
     words = text.split()
     filtered_words = [word for word in words if word not in stopwords]
     filtered_words = ' '.join(filtered_words)
 
-    # prépare les mots pour la lemmatization
     document = nlp(filtered_words)
     lemmas = [token.lemma_ for token in document]
 
-    # Filtrage des lemmes vides ou contenant uniquement des espaces
     filtered_lemmas = [lemma for lemma in lemmas if lemma.strip()]
 
     return filtered_lemmas
 
 
-# Remplis la liste des mots finaux
-cleaned_lemmas = []
-for paragraph in allText:
-    cleaned_paragraph_lemmas = clean_and_lemmatize(paragraph, stopwords)
-    cleaned_lemmas.extend(cleaned_paragraph_lemmas)
+def final_words(all_text, caracteres, stopwords, nlp): # Passer nlp_model ici aussi
+    cleaned_lemmas = []
+    for paragraph in all_text:
+        cleaned_paragraph_lemmas = clean_and_lemmatize(paragraph, caracteres, stopwords, nlp)
+        cleaned_lemmas.extend(cleaned_paragraph_lemmas)
 
-print(cleaned_lemmas)
+    return cleaned_lemmas
 
 
+if __name__ == "__main__":
+    record_ID = "test.docx"
+    nlp = spacy.load("fr_core_news_lg")
 
+    stopwords, caracteres, all_text = docx_to_list(record_ID)
+    final_word_list = final_words(all_text, caracteres, stopwords, nlp)
+    print(final_word_list)
